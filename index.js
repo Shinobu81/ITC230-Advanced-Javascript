d634f95e3f56d953c34138dd6e062f9fe69e4e51
Shinobu81 <shinobu.kinjo@gmail.com>
2019-05-06 22:26:33 -0700

    Assignment #3 Adds routes to detail and delete pages

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-const music = require("./lib/music.js"); // add music.j library
-const http = require("http");
-const fs = require("fs");
-const qs = require('querystring');
+'use strict'
 
-http.createServer((req,res) => {
-  let url = req.url.split("?");
-  let query = qs.parse(url[1]);
-  let path = url[0].toLowerCase();
-  switch(path) {
-    case '/':
-      fs.readFile("public/home.html", (err, data) => {
-      if (err) return console.error(err);
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data.toString());
-      });
-      console.log('HOME');
-      break;
-    case '/about':
-      fs.readFile("public/about.html", (err, data) => {
-      if (err) return console.error(err);
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data.toString());
-      });
-      console.log('ABOUT');
-      break;
-    case '/getall':
-      let search = music.getAll();
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end(JSON.stringify(search));
-      break;
-    case '/get':
-      let find = music.get(query.song);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results = (find) ? JSON.stringify(find) : "Not found";
-      res.end('Results for ' + query.song + "\n" + results);
-      break;
-    case '/delete':
-      let removed = music.delete(query.song);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let deleted = (removed) ? JSON.stringify(removed) : "Not found";
-      res.end('Results for ' + query.song + "\n" + 'deleted');
-      break;      
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('No music found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+const music = require('./lib/music.js');
 
-console.log('Server running port 3000');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+let handlebars = require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
+app.get('/', (req,res) => {
+  res.type('text/html');
+  res.render(__dirname + '/public/home.html'); 
+});
+
+app.get('/about', (req, res) => {
+  res.type('text/html');
+  res.render(__dirname + '/public/about.html');
+});
+
+app.get('/details', (req,res) => {
+  let result = music.get(req.query.song);
+  res.render('details', {song: req.query.song, result: result });
+});
+
+app.get('/delete', (req,res) => {
+  var result = music.delete(req.query.song);
+  res.render("delete", {song: req.query.song, result: result});
+});
+
+app.use((req,res) => {
+  res.type('text/plain'); 
+  res.status(404);
+  res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express started'); 
+});
