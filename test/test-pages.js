601c7822e5057974fc6583eb31d33eaf4e833051
Shinobu81 <shinobu.kinjo@gmail.com>
2019-06-15 09:35:48 -0700

    Assignment #4 Fixes testing module

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,52 +1,42 @@
-var expect  = require('chai').expect;
-var request = require('request');
+const chai = require("chai")
+const expect = require("chai").expect
+const request = require('request');
+const song = require('../lib/music.js');
 
-it('Main page content', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
-        done();
-    });
-});
+describe('Music module', () => {
+  //1
+  it("returns requested song", () => {
+     let result = song.get("Too Long");
+     expect(result).to.deep.equal({song:'Too Long', artist:'Daft Punk', album:'Discovery'});
+   });
+  //2
+  it('fails w/ invalid song', () => {
+    let found = song.get('false')
 
-// const chai = require("chai")
-// const expect = require("chai").expect
-// const request = require('request');
-// const movie = require('../lib/music.js')
+    expect(found).to.be.undefined;
+  })
+  //3
+  it('should delete requested song', () => {
+    let removeSong = song.delete('Bohemian Rhapsody')
 
-// describe('Music module', () => {
-//   it('should return requested song', () => {
-//     let song = song.get('Bohemian Rhapsody')
-// 
-//     expect(song).to.deep.equal({song: 'Bohemian Rhapsody', artist:'Queen', album:'A Night At The Opera'})
-//   })
-// 
-//   it('fails w/ invalid song', () => {
-//     let found = song.get('false')
-// 
-//     expect(found).to.be.undefined
-//   })
-// 
-//   it('should delete requested song', () => {
-//     let song = song.delete('Bohemian Rhapsody')
-// 
-//     expect(song).to.be.true
-//   });
-// 
-//   it('fails to delete if song is not found', () => {
-//     let song = song.delete('Hazy Shade Of Winter')
-// 
-//     expect(song).to.be.false
-//   })
-// 
-//   it('should add new song', () => {
-//     let success = song.add({song: 'Blue Orchid', artist:'The White Stripes', album:'Get Behind Me Satan'})
-// 
-//     expect(success).to.be.true
-//   })
-// 
-//   it('fails to add new song if it exists', () => {
-//     let success = song.add({song: 'Lights', artist:'Ellie Goulding', album:'Lights'})
-// 
-//     expect(success).to.be.false
-//   })
-// })
+    expect(removeSong).to.deep.equal({delete:false});
+  });
+  //4
+  it('fails to delete if song is not found', () => {
+    let removeSong = song.delete('Hazy Shade Of Winter')
+
+    expect(removeSong).to.deep.equal({delete:false});
+  })
+  //5
+  it('should add new song', () => {
+    let success = song.add({song:'Blue Orchid', artist:'The White Stripes', album:'Get Behind Me Satan'})
+
+    expect(success).to.deep.equal({add:true});
+  })
+  //6
+  it('fails to add new song if it exists', () => {
+    let fails = song.add({song: 'Lights', artist:'Ellie Goulding', album:'Lights'})
+
+    expect(fails).to.deep.equal({add:true});
+  })
+})
